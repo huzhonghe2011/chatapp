f6c2e08ad6ef42a93a79f95447ca10b633864cbb
huzhonghe2011 <huzhonghe2@outlook.com>
2025-01-06 19:53:59 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static('public'));
 
 // 定义一个连接事件，用于监听客户端的连接
 io.on('connection', (socket) => {
-  console.log('一个用户已连接');
+  console.log('一个用户连接: ' + socket.id);
   onlineUsers.add(socket.id);  // 用户连接时，把 socket.id 添加到集合中
   onlineCount = onlineUsers.size;  // 更新在线人数
 
@@ -28,6 +28,11 @@ io.on('connection', (socket) => {
 
   // 监听来自客户端的聊天消息
   socket.on('chatMessage', (msg) => {
+    const timestamp = new Date().toLocaleString(); // 获取当前时间
+    const userId = socket.id;  // 获取用户的 socket.id
+
+    console.log(`${msg.nickname} ${timestamp} 发送消息: ${msg.text}`);
+
     // 向所有客户端广播消息
     io.emit('chatMessage', msg);
   });
@@ -36,7 +41,7 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     onlineUsers.delete(socket.id);  // 用户断开时，从集合中移除 socket.id
     onlineCount = onlineUsers.size;  // 更新在线人数
-    console.log('一个用户断开');
+    console.log('一个用户断开连接: ' + socket.id);
     
     // 发送更新后的在线人数到所有客户端
     io.emit('onlineCount', onlineCount);
